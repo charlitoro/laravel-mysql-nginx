cf2ab5d7a4585025abd6f62905bf19ad4ed12b3c
Andres Toro <carlostoro04@gmail.com>
2020-11-20 22:26:44 -0500

    controllers and views

--- app/resources/views/catalog/show.blade.php
+++ app/resources/views/catalog/show.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.master')
  
 @section('content')
-     Show Movie {{$id}}
+     <div class="row"> 
+          <div class="col-sm-4">
+               <img src="{{ $movie['poster'] }}"/>
+          </div>
+          <div class="col-sm-8">
+               <h3>{{$movie['title']}}</h3>
+               <label> Año: {{$movie['year']}} </label>
+               <label> Director: {{$movie['director']}} </label>
+               <p><b>Synopsis:</b> {{$movie['synopsis']}}</p>
+               @if($movie['rented'] == true)
+                    <label><b>Status: </b>Pelicula rentada</label><br/>
+                    <button type="button" class="btn btn-primary">Devolver</button>
+               @else
+                    <label><b>Status: </b>Pelicula disponible</label><br/>
+                    <button type="button" class="btn btn-success">Rentar</button>
+               @endif
+               <button type="button" class="btn btn-info">Edital Pelicula</button>
+               <button type="button" class="btn btn-light">Volver</button>
+          </div>
+     </div>
 @stop
--- app/routes/web.php
+++ app/routes/web.php
@@ -13,27 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::get('/', function(){
-    return view( 'home' );
-});
-Route::get( 'login', function(){
-    return view( 'auth.login' );
-} );
-Route::get( 'logout', function(){
-    return 'Logout usuario';
-} );
-Route::get( 'catalog', function(){
-    return view( 'catalog.index' );
-} );
-Route::get( 'catalog/show/{id}', function($id){
-    return view( 'catalog.show', array('id' => $id) );
-} );
-Route::get( 'catalog/create', function(){
-    return view( 'catalog.create' );
-} );
-Route::get( 'catalog/edit/{id}', function($id){
-    return view( 'catalog.edit', array( 'id' => $id ) );
-} );
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
+
+Route::get('/', [HomeController::class, 'getHome']);
+Route::get( 'catalog', [CatalogController::class, 'getIndex'] );
+Route::get( 'catalog/show/{id}', [CatalogController::class, 'getShow'] );
+Route::get( 'catalog/create', [CatalogController::class, 'getCreate']);
+Route::get( 'catalog/edit/{id}', [CatalogController::class, 'getEdit'] );
+
+Route::get( 'login', function(){ return view( 'auth.login' ); } );
+Route::get( 'logout', function(){ return 'Logout usuario'; } );
